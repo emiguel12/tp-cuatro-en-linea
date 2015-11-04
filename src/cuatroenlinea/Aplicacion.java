c55941ebc220262bea9e74fdfecdeda429170b3d
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2015-11-03 23:10:17 -0300

    Modularización.

--- src/cuatroenlinea/Aplicacion.java
+++ src/cuatroenlinea/Aplicacion.java
@@ -6,54 +6,93 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Aplicacion extends Application {
 
+	private static final String TITULO = "Cuatro en Línea";
+	
+	private GridPane grilla;
+	
 	private TextField campoNombreJugadorRojo;
 	private TextField campoNombreJugadorAmarillo;
+
+	private Spinner<Integer> campoColumnas;
+	private Spinner<Integer> campoFilas;
+
+	private Button botonIniciar;
 
 	@Override
 	public void start(Stage escenarioPrincipal) {
 
-		GridPane grilla = new GridPane();
+		crearGrilla();
+
+		Scene escena = new Scene(grilla, 400, 300);
+		escenarioPrincipal.setScene(escena);
+		escenarioPrincipal.setTitle(TITULO);
+		escenarioPrincipal.show();
+	}
+
+	private void crearGrilla() {
+
+		grilla = new GridPane();
 		grilla.setAlignment(Pos.CENTER);
 		grilla.setHgap(20);
 		grilla.setVgap(20);
 		
-		campoNombreJugadorRojo = new TextField();
-		campoNombreJugadorAmarillo = new TextField();
-		Button botonIniciar = new Button("Iniciar");
-		botonIniciar.setOnAction(new Iniciar(this));
+		Text textoTitulo = new Text(TITULO);
+		textoTitulo.setFont(new Font(16));
 		
+		crearControles();
+
+		grilla.add(textoTitulo, 0, 0, 2, 1);
 		grilla.add(new Label("Jugador Rojo"), 0, 1);
 		grilla.add(campoNombreJugadorRojo, 1, 1);
 		grilla.add(new Label("Jugador Amarillo"), 0, 2);
 		grilla.add(campoNombreJugadorAmarillo, 1, 2);
-		grilla.add(botonIniciar, 0, 6, 2, 1);
+		grilla.add(new Label("Filas"), 0, 3);
+		grilla.add(campoFilas, 1, 3);
+		grilla.add(new Label("Columnas"), 0, 4);
+		grilla.add(campoColumnas, 1, 4);
+		grilla.add(botonIniciar, 0, 5, 2, 1);
+		
 		GridPane.setHalignment(botonIniciar, HPos.CENTER);
+		GridPane.setHalignment(textoTitulo, HPos.CENTER);
+	}
 
-		Scene escena = new Scene(grilla, 400, 300);
-		escenarioPrincipal.setScene(escena);
-		escenarioPrincipal.show();
+	private void crearControles() {
+
+		campoNombreJugadorRojo = new TextField();
+		campoNombreJugadorAmarillo = new TextField();
+		
+		campoColumnas = new Spinner<Integer>(4, 40, 7);
+		campoFilas = new Spinner<Integer>(4, 20, 7);
+		
+		botonIniciar = new Button("Iniciar");
+		botonIniciar.setOnAction(new Iniciar(this));
 	}
 	
 	public void iniciar() {
 		
 		String nombreJugadorRojo = campoNombreJugadorRojo.getText();
 		String nombreJugadorAmarillo = campoNombreJugadorAmarillo.getText();
+		int filas = campoFilas.getValue();
+		int columnas = campoColumnas.getValue();
 		
-		CuatroEnLinea juego = new CuatroEnLinea(7, 7, 
-												nombreJugadorRojo,
-												nombreJugadorAmarillo);
+		CuatroEnLinea juego = new CuatroEnLinea(filas, columnas, 
+												nombreJugadorRojo, nombreJugadorAmarillo);
 		
 		Tablero tablero = new Tablero(juego);
 		tablero.mostrar();
 	}
 	
 	public static void main(String[] args) {
+		
 		launch(args);
 	}
 }
