1acc67f3f6f80bef200a2d7c6576e55b4c23e494
Agus224 <massiminoagustin@gmail.com>
2020-11-06 17:53:57 -0300

    1234

--- src/juego/CuatroEnLinea.java
+++ src/juego/CuatroEnLinea.java
@@ -25,7 +25,7 @@ public class CuatroEnLinea {
 	 * @param jugadorAmarillo : nombre del jugador con fichas amarillas.
 	 */
 	public CuatroEnLinea(int filas, int columnas, String jugadorRojo, String jugadorAmarillo) {
-		double i = 32;
+		double i = 1234;
 		
 		
 	}
