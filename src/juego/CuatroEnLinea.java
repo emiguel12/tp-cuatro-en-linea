fd7a97d46e7db6201502a00d89f3b218ae8315f5
emiguel12 <miguelemmanuel@gmail.com>
2020-11-06 17:47:30 -0300

    volver a 0

---
+++ src/juego/CuatroEnLinea.java
@@ -0,0 +1,96 @@
+package juego;
+
+/**
+ * Juego Cuatro en Lí­nea
+ * 
+ * Reglas:
+ * 
+ * 		...
+ *
+ */
+public class CuatroEnLinea {
+	
+	private Casillero[][] tablero;
+
+	/**
+	 * pre : 'filas' y 'columnas' son mayores o iguales a 4.
+	 * post: empieza el juego entre el jugador que tiene fichas rojas, identificado como 
+	 * 		 'jugadorRojo' y el jugador que tiene fichas amarillas, identificado como
+	 * 		 'jugadorAmarillo'. 
+	 * 		 Todo el tablero está vacío.
+	 * 
+	 * @param filas : cantidad de filas que tiene el tablero.
+	 * @param columnas : cantidad de columnas que tiene el tablero.
+	 * @param jugadorRojo : nombre del jugador con fichas rojas.
+	 * @param jugadorAmarillo : nombre del jugador con fichas amarillas.
+	 */
+	public CuatroEnLinea(int filas, int columnas, String jugadorRojo, String jugadorAmarillo) {
+
+	}
+
+	/**
+	 * post: devuelve la cantidad máxima de fichas que se pueden apilar en el tablero.
+	 */
+	public int contarFilas() {
+		
+		return 4;
+	}
+
+	/**
+	 * post: devuelve la cantidad máxima de fichas que se pueden alinear en el tablero.
+	 */
+	public int contarColumnas() {
+		
+		return 4;
+	}
+
+	/**
+	 * pre : fila está en el intervalo [1, contarFilas()],
+	 * 		 columnas está en el intervalo [1, contarColumnas()].
+	 * post: indica qué ocupa el casillero en la posición dada por fila y columna.
+	 * 
+	 * @param fila
+	 * @param columna
+	 */
+	public Casillero obtenerCasillero(int fila, int columna) {
+		
+		return Casillero.VACIO;
+	}
+	
+	/**
+	 * pre : el juego no terminó, columna está en el intervalo [1, contarColumnas()]
+	 * 		 y aún queda un Casillero.VACIO en la columna indicada. 
+	 * post: deja caer una ficha en la columna indicada.
+	 * 
+	 * @param columna
+	 */
+	public void soltarFichaEnColumna(int columna) {
+		
+	}
+	
+	/**
+	 * post: indica si el juego terminó porque uno de los jugadores
+	 * 		 ganó o no existen casilleros vacíos.
+	 */
+	public boolean termino() {
+		
+		return false;
+	}
+
+	/**
+	 * post: indica si el juego terminó y tiene un ganador.
+	 */
+	public boolean hayGanador() {
+		
+		return false;
+	}
+
+	/**
+	 * pre : el juego terminó.
+	 * post: devuelve el nombre del jugador que ganó el juego.
+	 */
+	public String obtenerGanador() {
+		
+		return null;
+	}
+}
