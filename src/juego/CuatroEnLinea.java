cbef9f9d2b5c7b6ffcda6ee23cb465c71a62a270
Agus224 <massiminoagustin@gmail.com>
2020-11-09 23:46:40 -0300

    Horroroso metodo para ver ganador vertical

--- src/juego/CuatroEnLinea.java
+++ src/juego/CuatroEnLinea.java
@@ -5,58 +5,65 @@ package juego;
  * 
  * Reglas:
  * 
- * 		...
+ * ...
  *
  */
 public class CuatroEnLinea {
-	
+
 	private Casillero[][] tablero;
 	private String jugadorRojo;
 	private String jugadorAmarillo;
 	private String turno;
+	private String ganador;
 
 	/**
-	 * pre : 'filas' y 'columnas' son mayores o iguales a 4.
-	 * post: empieza el juego entre el jugador que tiene fichas rojas, identificado como 
-	 * 		 'jugadorRojo' y el jugador que tiene fichas amarillas, identificado como
-	 * 		 'jugadorAmarillo'. 
-	 * 		 Todo el tablero está vacío.
+	 * pre : 'filas' y 'columnas' son mayores o iguales a 4. post: empieza el
+	 * juego entre el jugador que tiene fichas rojas, identificado como
+	 * 'jugadorRojo' y el jugador que tiene fichas amarillas, identificado como
+	 * 'jugadorAmarillo'. Todo el tablero está vacío.
 	 * 
-	 * @param filas : cantidad de filas que tiene el tablero.
-	 * @param columnas : cantidad de columnas que tiene el tablero.
-	 * @param jugadorRojo : nombre del jugador con fichas rojas.
-	 * @param jugadorAmarillo : nombre del jugador con fichas amarillas.
+	 * @param filas
+	 *            : cantidad de filas que tiene el tablero.
+	 * @param columnas
+	 *            : cantidad de columnas que tiene el tablero.
+	 * @param jugadorRojo
+	 *            : nombre del jugador con fichas rojas.
+	 * @param jugadorAmarillo
+	 *            : nombre del jugador con fichas amarillas.
 	 */
-	public CuatroEnLinea(int filas, int columnas, String jugadorRojo, String jugadorAmarillo) {
+	public CuatroEnLinea(int filas, int columnas, String jugadorRojo,
+			String jugadorAmarillo) {
 
 		this.crearTablero(filas, columnas);
-		
+
 		this.jugadorAmarillo = jugadorAmarillo;
 		this.jugadorRojo = jugadorRojo;
-		
+
 		this.turno = "rojo";
 	}
 
 	/**
-	 * post: devuelve la cantidad máxima de fichas que se pueden apilar en el tablero.
+	 * post: devuelve la cantidad máxima de fichas que se pueden apilar en el
+	 * tablero.
 	 */
 	public int contarFilas() {
-		
+
 		return this.tablero.length;
 	}
 
 	/**
-	 * post: devuelve la cantidad máxima de fichas que se pueden alinear en el tablero.
+	 * post: devuelve la cantidad máxima de fichas que se pueden alinear en el
+	 * tablero.
 	 */
 	public int contarColumnas() {
-		
+
 		return this.tablero[0].length;
 	}
 
 	/**
-	 * pre : fila está en el intervalo [1, contarFilas()],
-	 * 		 columnas está en el intervalo [1, contarColumnas()].
-	 * post: indica qué ocupa el casillero en la posición dada por fila y columna.
+	 * pre : fila está en el intervalo [1, contarFilas()], columnas está en el
+	 * intervalo [1, contarColumnas()]. post: indica qué ocupa el casillero en
+	 * la posición dada por fila y columna.
 	 * 
 	 * @param fila
 	 * @param columna
@@ -64,67 +71,71 @@ public class CuatroEnLinea {
 	public Casillero obtenerCasillero(int fila, int columna) {
 		validarFila(fila);
 		validarColumna(columna);
-		return tablero[fila-1][columna-1];
+		return tablero[fila - 1][columna - 1];
 	}
-	
+
 	/**
-	 * pre : el juego no terminó, columna está en el intervalo [1, contarColumnas()]
-	 * 		 y aún queda un Casillero.VACIO en la columna indicada. 
+	 * pre : el juego no terminó, columna está en el intervalo [1,
+	 * contarColumnas()] y aún queda un Casillero.VACIO en la columna indicada.
 	 * post: deja caer una ficha en la columna indicada.
 	 * 
 	 * @param columna
 	 */
 	public void soltarFichaEnColumna(int columna) {
 		validarColumna(columna);
-		
+
 		int col = columna - 1;
-		int fila = this.contarFilas() -1;
+		int fila = this.contarFilas() - 1;
 		boolean soltoFicha = false;
-		
-		if(!termino()){
-			
-			while(fila >= 0 && soltoFicha == false){
-				
-				if(this.tablero[fila][col] != Casillero.VACIO){
-					fila --;
-				}else{
-					
-					this.tablero[fila][col] = (this.turno == "amarillo") ? Casillero.AMARILLO : Casillero.ROJO; 
+
+		if (!termino()) {
+
+			while (fila >= 0 && soltoFicha == false) {
+
+				if (this.tablero[fila][col] != Casillero.VACIO) {
+					fila--;
+				} else {
+
+					this.tablero[fila][col] = (this.turno == "amarillo") ? Casillero.AMARILLO
+							: Casillero.ROJO;
 					soltoFicha = true;
 				}
-				
+
 			}
-			
-			if(fila == -1){
+
+			if (fila == -1) {
 				return;
-			}else{
+			} else {
 				cambiarTurno();
 				return;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	/**
-	 * post: indica si el juego terminó porque uno de los jugadores
-	 * 		 ganó o no existen casilleros vacíos.
+	 * post: indica si el juego terminó porque uno de los jugadores ganó o no
+	 * existen casilleros vacíos.
 	 */
 	public boolean termino() {
+		if (hayGanador()) {
+			return true;
+		}
 		int casillerosOcupados = 0;
-		
-		for(int i = 0; i < this.tablero.length; i++){
-			for(int j = 0; j < this.tablero[i].length; j++){
-				if(this.tablero[i][j] != Casillero.VACIO){
+
+		for (int i = 0; i < this.tablero.length; i++) {
+			for (int j = 0; j < this.tablero[i].length; j++) {
+				if (this.tablero[i][j] != Casillero.VACIO) {
 					casillerosOcupados++;
 				}
 			}
 		}
-		
-		if(casillerosOcupados == casillerosTotales()){
+
+		if (casillerosOcupados == casillerosTotales()) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -132,71 +143,118 @@ public class CuatroEnLinea {
 	 * post: indica si el juego terminó y tiene un ganador.
 	 */
 	public boolean hayGanador() {
-		
+		if(ganadorVertical()){
+			return true;
+		}
+		 
 		return false;
 	}
 
 	/**
-	 * pre : el juego terminó.
-	 * post: devuelve el nombre del jugador que ganó el juego.
+	 * pre : el juego terminó. post: devuelve el nombre del jugador que ganó el
+	 * juego.
 	 */
 	public String obtenerGanador() {
-		
-		return null;
+
+		return ganador;
 	}
-	
-	private void crearTablero(int filas, int columnas){
-		
-		if(filas < 4 || filas > 10 ){
+
+	private void crearTablero(int filas, int columnas) {
+
+		if (filas < 4 || filas > 10) {
 			throw new Error("La cantidad de filas debe estar entre 4 y 10");
 		}
-		
-		if(columnas < 4 || columnas > 10){
+
+		if (columnas < 4 || columnas > 10) {
 			throw new Error("La cantidad de columnas debe estar entre 4 y 10");
 		}
-		
-		this.tablero = new Casillero[filas][columnas];  
+
+		this.tablero = new Casillero[filas][columnas];
 		this.inicializarTablero();
 	}
-	
-	private void inicializarTablero(){
-		
+
+	private void inicializarTablero() {
+
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-		      this.tablero[i][j] = Casillero.VACIO;	
+				this.tablero[i][j] = Casillero.VACIO;
 			}
 		}
 	}
-	
-	
-	private void cambiarTurno(){
-		
+
+	private void cambiarTurno() {
+
 		this.turno = (this.turno == "rojo") ? "amarillo" : "rojo";
 	}
-	
+
 	/*
 	 * POST: Analiza si la columna ingresada es válida
 	 */
-	private void validarColumna(int columna){
-		if(columna < 1 || columna > contarColumnas()){
-			throw new Error("La columna ingresada ("+columna +") es inválida.");
-			
+	private void validarColumna(int columna) {
+		if (columna < 1 || columna > contarColumnas()) {
+			throw new Error("La columna ingresada (" + columna
+					+ ") es inválida.");
+
 		}
 	}
+
 	/*
-	 *POST: Analiza si la fila ingresada es válida
+	 * POST: Analiza si la fila ingresada es válida
 	 */
-	private void validarFila(int fila){
-		if(fila < 1 || fila > contarFilas()){
+	private void validarFila(int fila) {
+		if (fila < 1 || fila > contarFilas()) {
 			throw new Error("La fila ingresada (" + fila + ") es inválida");
 		}
 	}
-	
+
 	/*
 	 * POST: Devuelve el total de casilleros del tablero
 	 */
-	
-	private int casillerosTotales(){
+
+	private int casillerosTotales() {
 		return (contarFilas() * contarColumnas());
 	}
+
+
+	private boolean ganadorVertical() {
+		int casillerosRojosSeguidos = 0;
+		int casillerosAmarillosSeguidos = 0;
+		
+		for (int i = 1; i < this.tablero.length; i++) {
+			for (int j = 0; j < this.tablero[i].length; j++) {
+				if(analizarCasillerosRojosVerticalesSeguidos(i,j))  {
+					casillerosRojosSeguidos++;
+				}
+				if (analizarCasillerosAmarillosVerticalesSeguidos(i,j)){
+					casillerosAmarillosSeguidos++;
+				}
+			}
+		}
+		if(casillerosRojosSeguidos >= 3){
+			ganador = this.jugadorRojo;
+			return true;
+		}else if(casillerosAmarillosSeguidos >= 3){
+			ganador = this.jugadorAmarillo;
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean analizarCasillerosRojosVerticalesSeguidos(int i, int j){
+		if(this.tablero[i][j] == this.tablero[i - 1][j]
+				&& this.tablero[i][j] != Casillero.VACIO && this.tablero[i][j] == Casillero.ROJO){
+					return true;
+	
+		}
+		return false;
+	}
+	
+	private boolean analizarCasillerosAmarillosVerticalesSeguidos(int i, int j){
+		if(this.tablero[i][j] == this.tablero[i - 1][j]
+				&& this.tablero[i][j] != Casillero.VACIO && this.tablero[i][j] == Casillero.AMARILLO){
+					return true;
+		}
+		return false;
+		}
+	
 }
