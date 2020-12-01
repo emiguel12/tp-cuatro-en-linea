c77cf3b14c224a1ae42ffe4238223f324ad77308
emiguel12 <miguelemmanuel@gmail.com>
2020-12-01 17:02:21 -0300

    Se agrego la documentacion del la clase CuatroEnLinea

--- src/juego/CuatroEnLinea.java
+++ src/juego/CuatroEnLinea.java
@@ -5,7 +5,21 @@ package juego;
  * 
  * Reglas:
  * 
- * ...
+ * El objetivo de este juego consiste en colocar cuatro fichas en una fila
+ * contínua vertical, horizontal o diagonalmente. Se juega sobre un tablero de
+ * nXm casillas que al empezar está vacío. Cómo colocar las fichas
+ * 
+ * Ambos jugadores sitúan sus fichas (una por movimiento) en el tablero. La
+ * regla para colocarlas consiste en que estas siempre "caen hasta abajo". Es
+ * decir una ficha puede ser colocada bien en la parte inferior de una columna o
+ * bien sobre otra de alguna otra columna.
+ * 
+ * La partida termina si una de las siguientes condiciones se cumple: Uno de los
+ * jugadores coloca cuatro o más fichas en una línea contínua vertical,
+ * horizontal o diagonalmente. Este jugador gana la partida.
+ *
+ * Todas las casillas del tablero están ocupadas y ningún jugador cumple la
+ * condición anterior para ganar. En este caso la partida finaliza en empate.
  *
  */
 public class CuatroEnLinea {
@@ -15,7 +29,6 @@ public class CuatroEnLinea {
 	private String jugadorAmarillo;
 	private String ganador;
 	private boolean esTurnoRojo = true;
-
 
 	/**
 	 * pre : 'filas' y 'columnas' son mayores o iguales a 4. post: empieza el
@@ -32,7 +45,8 @@ public class CuatroEnLinea {
 	 * @param jugadorAmarillo
 	 *            : nombre del jugador con fichas amarillas.
 	 */
-	public CuatroEnLinea(int filas, int columnas, String jugadorRojo,String jugadorAmarillo) {
+	public CuatroEnLinea(int filas, int columnas, String jugadorRojo,
+			String jugadorAmarillo) {
 
 		this.crearTablero(filas, columnas);
 		this.jugadorAmarillo = jugadorAmarillo;
@@ -64,7 +78,9 @@ public class CuatroEnLinea {
 	 * la posición dada por fila y columna.
 	 * 
 	 * @param fila
+	 *            : numero de fila
 	 * @param columna
+	 *            : numero de columna
 	 */
 	public Casillero obtenerCasillero(int fila, int columna) {
 		validarFila(fila);
@@ -75,35 +91,37 @@ public class CuatroEnLinea {
 	/**
 	 * pre : el juego no terminó, columna está en el intervalo [1,
 	 * contarColumnas()] y aún queda un Casillero.VACIO en la columna indicada.
-	 * post: deja caer una ficha en la columna indicada.
+	 * post: deja caer una ficha en la columna indicada y cambia el turno del
+	 * jugador.
 	 * 
 	 * @param columna
 	 */
 	public void soltarFichaEnColumna(int columna) {
-		
-		if(!this.termino()&& !this.esColumnaLlena(columna)){
-			
-			
+
+		if (!this.termino() && !this.esColumnaLlena(columna)) {
+
 			int i = 1;
-			while((i!= this.contarFilas()) && (this.obtenerCasillero(i+1,columna) == Casillero.VACIO)){
-				 i++;
+			while ((i != this.contarFilas())
+					&& (this.obtenerCasillero(i + 1, columna) == Casillero.VACIO)) {
+				i++;
 			}
 
-			this.tablero[i-1][columna-1] = this.esTurnoRojo ? Casillero.ROJO : Casillero.AMARILLO;
-			
-		    if (this.comprobar(i, columna, this.tablero[i-1][columna-1]))
-	    	{
-		    	this.ganador = this.esTurnoRojo ? this.jugadorRojo : this.jugadorAmarillo;
-	    	}
-			this.cambiarTurno();				
+			this.tablero[i - 1][columna - 1] = this.esTurnoRojo ? Casillero.ROJO
+					: Casillero.AMARILLO;
+
+			if (this.comprobar(i, columna, this.tablero[i - 1][columna - 1])) {
+				this.ganador = this.esTurnoRojo ? this.jugadorRojo
+						: this.jugadorAmarillo;
+			}
+			this.cambiarTurno();
 
 		}
 
 	}
 
 	/**
-	 * post: indica si el juego terminó porque uno de los jugadores ganó o no
-	 * existen casilleros vacíos.
+	 * post: indica si el juego terminó porque uno de los jugadores ganó o
+	 * existe empate.
 	 */
 	public boolean termino() {
 		if (hayGanador()) {
@@ -130,8 +148,7 @@ public class CuatroEnLinea {
 	 * post: indica si el juego terminó y tiene un ganador.
 	 */
 	public boolean hayGanador() {
-		if(this.ganador == null)
-		{
+		if (this.ganador == null) {
 			return false;
 		}
 		return true;
@@ -146,6 +163,15 @@ public class CuatroEnLinea {
 		return ganador;
 	}
 
+	/**
+	 * pre : 'filas' y 'columnas' son mayores o iguales a 4 y menores iguales a
+	 * 10. post: se crea el tablero vacio con tantas filas y columnas indicadas
+	 * 
+	 * @param filas
+	 *            : cantidad de filas que tiene el tablero.
+	 * @param columnas
+	 *            : cantidad de columnas que tiene el tablero.
+	 */
 	private void crearTablero(int filas, int columnas) {
 
 		if (filas < 4 || filas > 10) {
@@ -160,6 +186,11 @@ public class CuatroEnLinea {
 		this.inicializarTablero();
 	}
 
+	/**
+	 * pre : existe un tablero post: se inicializa cada casillero del tablero
+	 * con el Casillero VACIO
+	 * 
+	 */
 	private void inicializarTablero() {
 
 		for (int i = 0; i < tablero.length; i++) {
@@ -169,15 +200,25 @@ public class CuatroEnLinea {
 		}
 	}
 
-	private void cambiarTurno(){
-		
+	/**
+	 * 
+	 * post: Cambia el turno del jugador Rojo a el jugador Amarillo y viceversa
+	 * 
+	 */
+	private void cambiarTurno() {
+
 		this.esTurnoRojo = !this.esTurnoRojo;
 
 	}
-	
-	private boolean esColumnaLlena(int columna){
-		
-		if(this.obtenerCasillero(1, columna) != Casillero.VACIO){
+
+	/**
+	 * pre : existe la columna en el tablero post: indica si la columna esta sin
+	 * casilleros disponibles
+	 * 
+	 */
+	private boolean esColumnaLlena(int columna) {
+
+		if (this.obtenerCasillero(1, columna) != Casillero.VACIO) {
 			return true;
 		}
 		return false;
@@ -211,112 +252,177 @@ public class CuatroEnLinea {
 		return (contarFilas() * contarColumnas());
 	}
 
+	/**
+	 * pre : 'filas' y 'columnas' pertenecen al tablero. post: indica si el
+	 * jugador alineo 4 Fichas en forma vertical
+	 * 
+	 * @param fila
+	 *            : numero de fila de la ultima ficha ingresada
+	 * @param columna
+	 *            : numero de columna de la ultima ficha ingresada
+	 * @param casillero
+	 *            : Casillero del color correspondiente al jugador
+	 */
+	private boolean lineaVertical(int fila, int columna, Casillero casillero) {
 
-	private boolean lineaVertical(int fila, int columna, Casillero casillero){
-	
-		int filaMin = (fila - 3) >= 1 ? fila-3 : 1;
-		int filaMax = (fila + 3) <= this.contarFilas() ? fila+3:this.contarFilas();
+		int filaMin = (fila - 3) >= 1 ? fila - 3 : 1;
+		int filaMax = (fila + 3) <= this.contarFilas() ? fila + 3 : this
+				.contarFilas();
 		int cantFichasJuntas = 0;
-		
+
 		for (int i = filaMin; i <= filaMax; i++) {
-			
-			if(this.obtenerCasillero(i, columna) == casillero){
+
+			if (this.obtenerCasillero(i, columna) == casillero) {
 				cantFichasJuntas++;
-			}else{
-				cantFichasJuntas = cantFichasJuntas == 4 ? 4:0;
+			} else {
+				cantFichasJuntas = cantFichasJuntas == 4 ? 4 : 0;
 			}
-			
+
 		}
-		
-		return cantFichasJuntas>=4;
-		
+
+		return cantFichasJuntas >= 4;
+
 	}
-	
-	private boolean lineaHorizontal(int fila, int columna, Casillero casillero){
-		
-		int columnaMin = (columna - 3) >= 1 ? columna-3 : 1;
-		int columnaMax = (columna + 3) <= this.contarColumnas() ? columna+3 : this.contarColumnas();
+
+	/**
+	 * pre : 'filas' y 'columnas' pertenecen al tablero. post: indica si el
+	 * jugador alineo 4 Fichas en forma horizontal
+	 * 
+	 * @param fila
+	 *            : numero de fila de la ultima ficha ingresada
+	 * @param columna
+	 *            : numero de columna de la ultima ficha ingresada
+	 * @param casillero
+	 *            : Casillero del color correspondiente al jugador
+	 */
+	private boolean lineaHorizontal(int fila, int columna, Casillero casillero) {
+
+		int columnaMin = (columna - 3) >= 1 ? columna - 3 : 1;
+		int columnaMax = (columna + 3) <= this.contarColumnas() ? columna + 3
+				: this.contarColumnas();
 		int cantFichasJuntas = 0;
-		
+
 		for (int i = columnaMin; i <= columnaMax; i++) {
-			
-			if(this.obtenerCasillero(fila, i) == casillero){
+
+			if (this.obtenerCasillero(fila, i) == casillero) {
 				cantFichasJuntas++;
-			}else{
-				cantFichasJuntas = (cantFichasJuntas == 4) ?  4:0 ;
+			} else {
+				cantFichasJuntas = (cantFichasJuntas == 4) ? 4 : 0;
 			}
-			
+
 		}
-		
-		return cantFichasJuntas>=4;
-		
+
+		return cantFichasJuntas >= 4;
+
 	}
 
-private boolean lineaDiagonalDescendente(int fila, int columna, Casillero casillero){
-	
-	int filaMax = this.contarFilas();
-	int columnaMax = this.contarColumnas();
-	
-	//Contador de fichas
-	int cantFichasJuntas = 1; 
-	boolean seguirContandoIzq = true;
-	boolean seguirContandoDer = true;
-	int i = 1;
-	//Verificador por filas
-	while( (cantFichasJuntas<4 ) && (seguirContandoDer || seguirContandoIzq) ){
-		
-		if((seguirContandoIzq)&&(fila-i>=1 && columna-i>=1) && (this.obtenerCasillero(fila-i, columna-i) == casillero) ){
-			cantFichasJuntas++;
-		}
-		else{
-			seguirContandoIzq = false;
-		}
-		if((fila+i<=filaMax && columna+i<=columnaMax) && (this.obtenerCasillero(fila+i, columna+i) == casillero) ){
-			cantFichasJuntas++;
-		}else{
-			seguirContandoDer = false;
-		}
-		i++;
-	}
-	
-	return cantFichasJuntas >= 4;
-}
+	/**
+	 * pre : 'filas' y 'columnas' pertenecen al tablero. post: indica si el
+	 * jugador alineo 4 Fichas en forma diagonal descendente
+	 * 
+	 * @param fila
+	 *            : numero de fila de la ultima ficha ingresada
+	 * @param columna
+	 *            : numero de columna de la ultima ficha ingresada
+	 * @param casillero
+	 *            : Casillero del color correspondiente al jugador
+	 */
+	private boolean lineaDiagonalDescendente(int fila, int columna,
+			Casillero casillero) {
 
-private boolean lineaDiagonalAscendente(int fila, int columna, Casillero casillero){
-	
-	int filaMax = this.contarFilas();
-	int columnaMax = this.contarColumnas();
-	
-	//Contador de fichas
-	int cantFichasJuntas = 1; 
-	boolean seguirContandoIzq = true;
-	boolean seguirContandoDer = true;
-	int i = 1;
-	//Verificador por filas
-	while( (cantFichasJuntas<4 ) && (seguirContandoDer || seguirContandoIzq) ){
-		
-		if((seguirContandoIzq)&&(fila+i<=filaMax && columna-i>=1) && (this.obtenerCasillero(fila+i, columna-i) == casillero) ){
-			cantFichasJuntas++;
-		}
-		else{
-			seguirContandoIzq = false;
-		}
-		if((fila-i>=1 && columna+i<=columnaMax) && (this.obtenerCasillero(fila-i, columna+i) == casillero) ){
-			cantFichasJuntas++;
-		}else{
-			seguirContandoDer = false;
-		}
-		i++;
-	}
-	
-	return cantFichasJuntas >= 4;
-}
+		int filaMax = this.contarFilas();
+		int columnaMax = this.contarColumnas();
 
-	private boolean comprobar(int fila,int columna, Casillero casillero){
-		
-		return (this.lineaHorizontal(fila, columna, casillero) || this.lineaVertical(fila, columna, casillero) || 
-				lineaDiagonalAscendente(fila, columna, casillero)|| lineaDiagonalDescendente(fila, columna, casillero));
-		
-		
+		// Contador de fichas
+		int cantFichasJuntas = 1;
+		boolean seguirContandoIzq = true;
+		boolean seguirContandoDer = true;
+		int i = 1;
+		// Verificador por filas
+		while ((cantFichasJuntas < 4)
+				&& (seguirContandoDer || seguirContandoIzq)) {
+
+			if ((seguirContandoIzq)
+					&& (fila - i >= 1 && columna - i >= 1)
+					&& (this.obtenerCasillero(fila - i, columna - i) == casillero)) {
+				cantFichasJuntas++;
+			} else {
+				seguirContandoIzq = false;
+			}
+			if ((fila + i <= filaMax && columna + i <= columnaMax)
+					&& (this.obtenerCasillero(fila + i, columna + i) == casillero)) {
+				cantFichasJuntas++;
+			} else {
+				seguirContandoDer = false;
+			}
+			i++;
+		}
+
+		return cantFichasJuntas >= 4;
+	}
+
+	/**
+	 * pre : 'filas' y 'columnas' pertenecen al tablero. post: indica si el
+	 * jugador alineo 4 Fichas en forma diagonal ascendente
+	 * 
+	 * @param fila
+	 *            : numero de fila de la ultima ficha ingresada
+	 * @param columna
+	 *            : numero de columna de la ultima ficha ingresada
+	 * @param casillero
+	 *            : Casillero del color correspondiente al jugador
+	 */
+	private boolean lineaDiagonalAscendente(int fila, int columna,
+			Casillero casillero) {
+
+		int filaMax = this.contarFilas();
+		int columnaMax = this.contarColumnas();
+
+		// Contador de fichas
+		int cantFichasJuntas = 1;
+		boolean seguirContandoIzq = true;
+		boolean seguirContandoDer = true;
+		int i = 1;
+		// Verificador por filas
+		while ((cantFichasJuntas < 4)
+				&& (seguirContandoDer || seguirContandoIzq)) {
+
+			if ((seguirContandoIzq)
+					&& (fila + i <= filaMax && columna - i >= 1)
+					&& (this.obtenerCasillero(fila + i, columna - i) == casillero)) {
+				cantFichasJuntas++;
+			} else {
+				seguirContandoIzq = false;
+			}
+			if ((fila - i >= 1 && columna + i <= columnaMax)
+					&& (this.obtenerCasillero(fila - i, columna + i) == casillero)) {
+				cantFichasJuntas++;
+			} else {
+				seguirContandoDer = false;
+			}
+			i++;
+		}
+
+		return cantFichasJuntas >= 4;
+	}
+
+	/**
+	 * pre : 'fila' y 'columna' pertenecen al tablero. post: indica si el
+	 * jugador alineo 4 Fichas en cualquier direccion
+	 * 
+	 * @param fila
+	 *            : numero de fila de la ultima ficha ingresada
+	 * @param columna
+	 *            : numero de columna de la ultima ficha ingresada
+	 * @param casillero
+	 *            : Casillero del color correspondiente al jugador
+	 */
+	private boolean comprobar(int fila, int columna, Casillero casillero) {
+
+		return (this.lineaHorizontal(fila, columna, casillero)
+				|| this.lineaVertical(fila, columna, casillero)
+				|| lineaDiagonalAscendente(fila, columna, casillero) || lineaDiagonalDescendente(
+					fila, columna, casillero));
+
 	}
 }
